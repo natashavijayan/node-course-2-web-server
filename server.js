9396b86be3ff3d029dd17563d648871541c7f1fb
Natasha Vijayan <natashavijayan@gmail.com>
2017-07-18 19:42:42 +0530

    Add Project Page

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 
 var app = express();
 
@@ -39,7 +39,13 @@ app.get('/',(req,res) => {
 
 app.get('/about',(req,res) => {
   res.render('about.hbs',{
-    pageTitle : 'About page',
+    pageTitle : 'About page'
+  });
+});
+
+app.get('/projects',(req,res) => {
+  res.render('projects.hbs',{
+    pageTitle : 'Projects page'
   });
 });
 
